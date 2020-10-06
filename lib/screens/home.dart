c3f025194562b54604eecb8c7ba90bff0c0e7ec8
Esam Olwan <lands.of.48@gmail.com>
2020-10-06 15:09:18 +0300

    first row is done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,6 +16,7 @@ class _HomePageState extends State<HomePage> {
             height: 42,
           ),
           Container(
+            //Leader top center text
             height: 97,
             width: 414,
             child: Center(
@@ -30,71 +31,139 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           SizedBox(height: 14),
-          Expanded(
-            child: Container(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      //Send a message
-                      Container(
-                        height: 110,
-                        width: 180,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(
-                            color: Colors.grey,
-                          ),
+          Container(
+            height: 242,
+            width: 378,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      //Send text Container
+                      width: 180,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          width: 2,
+                          color: borderColor,
                         ),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              height: 10,
-                              width: 10,
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 10,
+                            width: 10,
+                          ),
+                          Container(
+                            //Send text image
+                            height: 61,
+                            width: 82,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/kite.png'),
+                                  fit: BoxFit.fill),
                             ),
-                            Image.asset('assets/images/kite.png'),
-                            SizedBox(
-                              height: 24,
-                              width: 24,
-                            ),
-                            Container(
-                              height: 58,
-                              width: 46,
-                              child: Text(
-                                'إرسال رسالة',
-                                style: TextStyle(
-                                  fontFamily: 'Calibri',
-                                  fontSize: 20,
-                                ),
+                          ),
+                          SizedBox(
+                            height: 24,
+                            width: 24,
+                          ),
+                          Container(
+                            // Send message text
+                            height: 58,
+                            width: 46,
+                            child: Text(
+                              'إرسال رسالة',
+                              style: TextStyle(
+                                color: borderColor,
+                                fontFamily: 'Calibri',
+                                fontSize: 20,
                               ),
                             ),
-                          ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      // Charge balance
+                      width: 180,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          width: 2,
+                          color: borderColor,
                         ),
                       ),
-                      SizedBox(
-                        width: 18,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 24,
+                            width: 18,
+                          ),
+                          Container(
+                            height: 58,
+                            width: 73,
+                            child: Text(
+                              'شحن الرصيد',
+                              style: TextStyle(
+                                color: borderColor,
+                                fontFamily: 'Calibri',
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          Container(
+                            height: 70,
+                            width: 65,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/chargeBalance.png'),
+                                  fit: BoxFit.fill),
+                            ),
+                          ),
+                        ],
                       ),
-                      Container(
-                        // charge balance
-                        height: 110,
-                        width: 180,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            border: Border.all(
-                              color: Colors.grey,
-                            )),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              color: Colors.white,
-              height: 242,
-              width: 378,
-              margin: EdgeInsets.only(
-                right: 18,
-                left: 18,
-              ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 22,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 180,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          width: 2,
+                          color: borderColor,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 180,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          width: 2,
+                          color: borderColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           )
         ],
